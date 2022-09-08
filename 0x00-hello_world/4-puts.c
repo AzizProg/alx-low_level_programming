7e14528655183c705d77db2361411525a4787ec8
AzizProg <traoreabdoulaziz70@gmail.com>
2022-09-08 14:02:03 -0700

    All

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *
+ * main-entry
+ *
+ * return: 0 end
+ * */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *
+ * main-Entry
+ *
+ * return: always 0 (Success)
+ */
+int main(void)
+{
+	print("with proper grammer, but the outcome is a place of art,\n");
+	return (0);
+}
