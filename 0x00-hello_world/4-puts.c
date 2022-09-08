f158cdd11fd8301265c0c2409396c39bef2e2e15
AzizProg <traoreabdoulaziz70@gmail.com>
2022-09-08 14:24:57 -0700

    z

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  *
- * main-entry
+ * main - print the string in the put function
  *
- * return: 0 end
+ *Description: using the main function 
+ *this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  * */
 int main (void)
 {
